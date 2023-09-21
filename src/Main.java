72b298a6a93e17aabc1c1f907972b8c50a8dbb65
zhushanbo <zhushanbo@baidu.com>
2023-09-21 21:08:18 +0800

    no message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-public class TestJava {
+public class Main {
     // 这里是程序入口
     public static void main(String[] args) {
         // 输出字符串
-        System.out.println("你好 Java");
+        System.out.println("Hello world!");
     }
 }
