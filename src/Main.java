6eaa852d29bbb868be2f1c8ad92a7f6e3db5140f
zhushanbo <zhushanbo@baidu.com>
2024-06-24 17:03:10 +0800

    no message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
-    // 这里是程序入口
+    public String name; // 姓名
+    public int age; // 年龄
+    // 定义构造方法，为属性初始化
+    public Main(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    // 获取信息的方法
+    public void tell() {
+        System.out.println("姓名：" + name + "，年龄：" + age);
+        System.out.println("姓名：" + this.name + "，年龄：" + this.age);
+    }
     public static void main(String[] args) {
-        // 输出字符串
-        System.out.println("Hello world!");
+        new Main("张三", 30).tell(); // 匿名对象
     }
 }
