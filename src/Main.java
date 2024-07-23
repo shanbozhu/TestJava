aade18a3dc90ca44ba0b84e371c61dfff557ddcd
zhushanbo <zhushanbo@baidu.com>
2024-07-23 19:48:57 +0800

    no message

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
 public class Main {
+    public final double PI = 3.14;
+    final  int a = 10;
     public String name; // 姓名
     public int age; // 年龄
+
     // 定义构造方法，为属性初始化
     public Main(String name, int age) {
         this.name = name;
         this.age = age;
     }
+
     // 获取信息的方法
     public void tell() {
         System.out.println("姓名：" + name + "，年龄：" + age);
         System.out.println("姓名：" + this.name + "，年龄：" + this.age);
     }
+
     public static void main(String[] args) {
-        new Main("张三", 30).tell(); // 匿名对象
+        Main obj = new Main("张三", 30);
+        obj.tell(); // 匿名对象
+        System.out.println(
+                obj.a
+        );
     }
 }
---
+++ src/day1/DataClass.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class DataClass {
+    String name; // 属性
+    int age; // 属性
+    static final String website = "C语言中文网"; // 类属性
+    static String URL = "http://c.biancheng.net"; // 类属性
+}
